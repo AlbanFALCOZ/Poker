582851b84724db45a8caea4c76eda5e5061cdaae
Nora Kaymakcilar <norakaymakcilar@MacBook-Air-de-Nora.local>
2023-10-27 16:50:25 +0200

    méthode pair

--- Poker-s5/src/Poker/MainPoker.java
+++ Poker-s5/src/Poker/MainPoker.java
@@ -3,6 +3,45 @@ package Poker;
 import java.util.Scanner;
 
 public class MainPoker {
-    
+
+    Carte[] main;
+    int[] occurrences = new int[14];
+
+    public MainPoker(Carte[] main){
+        this.main=main;
+    }
+
+    public boolean pair() {
+        for (Carte carte : main) { //parcourir chaque carte de la main
+            int indice = carte.getValue()-2;
+            occurrences[indice] += 1;
+
+            if (occurrences[indice] == 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        Carte[] testMain = {
+                new Carte(3,"Tr"),
+                new Carte(4,"Tr"),
+                new Carte(6,"Tr"),
+                new Carte(8,"Tr"),
+                new Carte(10,"Tr")
+        };
+        MainPoker pokerMain = new MainPoker(testMain);
+        System.out.println(pokerMain.pair()); // devrait retourner true
+    }
+
 
 }
