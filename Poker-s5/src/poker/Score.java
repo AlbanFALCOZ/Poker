072297c11416809401364bbfa919230ed29a4f7a
Gholab <hajarelgholabzouri@gmail.com>
2023-11-07 12:11:20 +0100

    bugs conflits, changement de hauteSuite

--- Poker-s5/src/poker/Score.java
+++ Poker-s5/src/poker/Score.java
@@ -12,20 +12,6 @@ public class Score {
     }
 
 
-    private void initializeScoreRules() {           //score pour chaque règle
-        scoreParRule.put("HighCard", 0);
-        scoreParRule.put("Pair", 1);
-        scoreParRule.put("DeuxPairs", 2);
-        scoreParRule.put("Brelan", 3);
-        scoreParRule.put("Suite", 4);
-        scoreParRule.put("Couleur", 5);
-        scoreParRule.put("Full", 6);
-        scoreParRule.put("Carre", 7);
-        //scoreParRule.put("StraightFlush", 8);
-        //scoreParRule.put("RoyalFlush", 9);
-    }
-
-
 
 
 }
