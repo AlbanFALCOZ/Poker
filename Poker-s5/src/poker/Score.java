b1fab0ac1ded6e922b87a6dabc1e8bbf999d1755
Gholab <hajarelgholabzouri@gmail.com>
2023-11-10 15:14:20 +0100

    Modification calculateScore classe Score

--- Poker-s5/src/poker/Score.java
+++ Poker-s5/src/poker/Score.java
@@ -21,10 +21,11 @@ public class Score {
         scoreByRule.put("Couleur", 5);
         scoreByRule.put("Full", 6);
         scoreByRule.put("Carre", 7);
+        scoreByRule.put("QuinteFlush",8);
     }
 
 
-    public void calculateScoreHand() {         //déterminer le score de la main
+    public int calculateScoreHand() {         //déterminer le score de la main
         if (Hand.carre()) scoreHand = scoreByRule.get("Carre");
         else if (Hand.full()) scoreHand = scoreByRule.get("Full");
         else if (Hand.couleur()) scoreHand = scoreByRule.get("Couleur");
@@ -32,11 +33,14 @@ public class Score {
         else if (Hand.brelan()) scoreHand = scoreByRule.get("Brelan");
         else if (Hand.deuxPaires()) scoreHand = scoreByRule.get("DeuxPairs");
         else if (Hand.pair()) scoreHand = scoreByRule.get("Pair");
+        else if (Hand.quinteFlush()) scoreHand= scoreByRule.get("QuinteFlush");
         else scoreHand = scoreByRule.get("HighCard"); //aucune combinaison, carte haute
-    }
 
-
-    public int getScoreHand() {
         return scoreHand;
     }
+
+
+
+
+
 }
